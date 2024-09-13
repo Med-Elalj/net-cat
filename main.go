5e7724cae8716b029eb5bd684252735a8265ccc0
fennadaf <fatimaezzahraennadafy@gmail.com>
2024-09-13 12:39:59 +0100

    done

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
 var (
@@ -19,15 +23,53 @@ func main() {
 		startServer()
 		return
 	}
+	if len(flag.Args()) < 2 {
+		fmt.Println("Hostname and port required")
+		return
+	}
+	serverHost := flag.Arg(0)
+	serverPort := flag.Arg(1)
+	startClient(fmt.Sprintf("%s:%s", serverHost, serverPort))
 }
 
 func startServer() {
+	// donc addr is server's address
 	addr := fmt.Sprintf("%s:%d", *host, *port)
 	// launch TCP server
-	// listener, err := net.Listen("tcp", addr)
-	// if err != nil {
-	// 	// if we can't launch server for some reason,
-	// 	panic(err)
-	// }
-	fmt.Println(addr)
+	// func net.Listen  It is used to create a network listener that waits for incoming connections on a specified network address and port
+	// net.Listen("tcp" the network type , addr the address and port to listen on)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		// if we can't launch server for some reason,
+		panic(err)
+	}
+
+	// the infinite loop accepts TCP connections from clients and processes these connections in separate goroutines
+	for {
+		// The listener.Accept() method in Go is used to accept a new incoming connection on a network listener.
+		// after each successful connection ( by processClient(conn) ) , server returns to the listener.Accept() and gets ready for the next client.
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection from client: %s", err)
+		} else {
+			// Handle the connection in a new goroutine
+			go processClient(conn)
+		}
+	}
+}
+
+// processing's function
+// io.Copy way to copy data from one source to a destination.
+// conn from which the data will be read (Copying Data from a Network Connection)
+// os.Stdout where the data will be copied to. type : standard output which is typically the terminal
+func processClient(conn net.Conn) {
+	_, err := io.Copy(os.Stdout, conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn.Close()
+}
+
+func startClient(addr string) {
+	//net.Dial  
 }
