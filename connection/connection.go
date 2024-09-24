e782851027638067500e67256cff76dd824daac1
melalj <mohammedelalj@outlook.com>
2024-09-24 15:39:26 +0100

    done un confirmed audit

--- connection/connection.go
+++ connection/connection.go
@@ -18,14 +18,19 @@ func BrodCast(msg string, conn net.Conn) {
 		if client != conn {
 			client.Write([]byte(msg))
 		}
-		client.Write([]byte("[" + currentTime + "]" + "[" + name + "]:"))
+		client.Write([]byte(fmt.Sprintf("[%s][%s]:", currentTime, name)))
 	}
 }
 
-func Connection(conn net.Conn, File []byte) {
+func Connection(conn net.Conn, Logo []byte) {
 	defer conn.Close()
+	defer delete(Clients, conn)
 	conn.Write([]byte("Welcome to TCP-Chat!\n"))
-	conn.Write([]byte(File))
+	conn.Write([]byte(Logo))
+	if len(Clients) > 10 {
+		conn.Write([]byte("sorry server full cannot join the chat"))
+		return
+	}
 	name := ""
 	read := bufio.NewReader(conn)
 	for {
@@ -42,36 +47,27 @@ func Connection(conn net.Conn, File []byte) {
 	Clients[conn] = name
 	mu.Unlock()
 	fmt.Println(len(Clients))
-	if len(Clients) >= 2 {
-		conn.Write([]byte("you cannot join the chat"))
-		return
-	}
-
 	if len(msgs) != 0 {
 		conn.Write([]byte(strings.Join(msgs, "") + "\n"))
-		conn.Write([]byte(fmt.Sprintf("[%s] [%s]:", time.Now().Format(time.DateTime), Clients[conn])))
-		// mssags = ""
 	}
 
-	BrodCast("\n"+Clients[conn]+" has joined our chat..."+"\n", conn)
-	// mssags += "\n" + Clients[conn] + " has joined our chat..."
+	BrodCast("\n"+name+" has joined our chat..."+"\n", conn)
 	for {
 		msg, err := read.ReadString('\n')
 		if err != nil {
-			delete(Clients, conn)
 			BrodCast("\n"+name+" has left our chat...\n", conn)
 			break
 		}
 		if len(msg) == 1 {
 			conn.Write([]byte("you can't enter in empty message\n"))
-			conn.Write([]byte(fmt.Sprintf("[%s] [%s]:", time.Now().Format(time.DateTime), Clients[conn])))
+			conn.Write([]byte(fmt.Sprintf("[%s][%s]:", time.Now().Format(time.DateTime), Clients[conn])))
 			continue
 		}
 		if !isPrintable(msg[:len(msg)-1]) {
 			conn.Write([]byte("you just entered in invalid text\n"))
-			conn.Write([]byte(fmt.Sprintf("[%s] [%s]:", time.Now().Format(time.DateTime), Clients[conn])))
+			conn.Write([]byte(fmt.Sprintf("[%s][%s]:", time.Now().Format(time.DateTime), Clients[conn])))
 			continue
 		}
-		BrodCast(fmt.Sprintf("\n[%s] [%s]:%s", time.Now().Format(time.DateTime), Clients[conn], msg), conn)
+		BrodCast(fmt.Sprintf("\n[%s][%s]:%s", time.Now().Format(time.DateTime), Clients[conn], msg), conn)
 	}
 }
